cda4b17d5a0b3f8d23db1aee608e4efba3a1acd5
Larfg <72176664+Larfg@users.noreply.github.com>
2022-04-14 22:53:23 -0500

    Creación del Bean ConsultarRecursosBean

--- src/main/java/managedbeans/ConsultarRecursosBean.java
+++ src/main/java/managedbeans/ConsultarRecursosBean.java
@@ -5,22 +5,26 @@ import com.google.inject.Inject;
 import entities.Recurso;
 import services.RecursosBiblioteca;
 
-
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
 import java.util.List;
 
-@ManagedBean(name = "ConsultarRecursos")
-@ApplicationScoped
-public class ConsultarRecursosBean extends BasePageBean{
+/**
+ * Bean para la interfaz de usuario de la consulta de recursos
+ */
+
+@ManagedBean(name = "consultarRecursosBean")
+@SessionScoped
+public class ConsultarRecursosBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     @Inject
     private RecursosBiblioteca recursosBiblioteca;
-    private Recurso recurso;
 
-/*
-    public List<Recurso> consultarRecurso() {
+    private Recurso recurso;
+    public List<Recurso> getRecursos() {
         return recursosBiblioteca.consultarRecursos();
     }
-    */
 
 }
