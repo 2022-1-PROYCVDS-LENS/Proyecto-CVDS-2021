fde510ea17947451b6558f501777de4be5ff9987
larfg <luis.giraldo-r@mail.escuelaing.edu.co>
2022-05-07 16:30:02 -0500

    conectado con login, se pueden recuperar los usuarios solo falta la tabla

--- src/main/java/managedbeans/VisualizarReservaBean.java
+++ src/main/java/managedbeans/VisualizarReservaBean.java
@@ -1,5 +1,7 @@
 package managedbeans;
 
+
+import com.google.inject.Inject;
 import entities.Recurso;
 import entities.Reserva;
 import entities.Usuario;
@@ -8,11 +10,9 @@ import services.RecursosBiblioteca;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.inject.Inject;
 import java.sql.Time;
 import java.util.List;
-import java.util.Map;
+
 
 @SuppressWarnings("deprecation")
 @ManagedBean(name = "visualizarReservaBean")
@@ -21,20 +21,19 @@ public class VisualizarReservaBean extends BasePageBean{
     @ManagedProperty(value = "#{loginBean}")
     private LoginBean loginBean;
 
-
     @Inject
-    private RecursosBiblioteca rebi;
+    private RecursosBiblioteca recursosBiblioteca;
+
     Recurso recurso;
-
-
-
     Time inicio = new Time(System.currentTimeMillis());
     Time fin;
     boolean recurrente;
     String estado;
     private Usuario user;
+    private int userId;
     String solicitud;
     List<Reserva> reservas;
+    Reserva reserva;
 
     public void setLoginBean(LoginBean loginBean) {
         this.loginBean = loginBean;
@@ -42,27 +41,43 @@ public class VisualizarReservaBean extends BasePageBean{
 
     public void init(){
         this.user = loginBean.getUser();
+        reservas = recursosBiblioteca.consultarReservasActivas(user.getId());
     }
 
     /**
      * Asigna a reservas las activas del usuario
      */
     public void consAct(){
-        reservas= rebi.consultarReservasActivas(user.getId());
+        reservas= recursosBiblioteca.consultarReservasActivas(user.getId());
     }
 
     /**
      * Asigna a reservas las canceladas del usuario
      */
     public void consCanc(){
-        reservas = rebi.consultarReservasCanceladas(user.getId());
+        reservas = recursosBiblioteca.consultarReservasCanceladas(user.getId());
     }
 
     /**
      * Asigna a reservas las pasadas del usuario
      */
     public void consPas(){
-        reservas = rebi.consultarReservasPasadas(user.getId());
+        reservas = recursosBiblioteca.consultarReservasPasadas(user.getId());
     }
 
+    public List<Reserva> getReservas() {
+        return reservas;
+    }
+
+    public void setReservas(List<Reserva> reservas) {
+        this.reservas = reservas;
+    }
+
+    public Reserva getReserva() {
+        return reserva;
+    }
+
+    public void setReserva(Reserva reserva) {
+        this.reserva = reserva;
+    }
 }
