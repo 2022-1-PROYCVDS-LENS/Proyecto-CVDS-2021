5c1e221ada9d8cb6fc95b0c12f5e19c82a0dcc36
ser0710 <serroz2822@gmail.com>
2022-05-10 11:02:37 -0500

    cambios

--- src/main/java/entities/Reserva.java
+++ src/main/java/entities/Reserva.java
@@ -16,16 +16,19 @@ import java.util.List;
 @AllArgsConstructor
 public class Reserva implements Serializable {
     private @Getter @Setter int id;
-    private @Getter @Setter int idUsuario;
-    private @Getter @Setter int idRecurso;
+    private @Getter @Setter Usuario idUsuario;
+    private @Getter @Setter Recurso idRecurso;
     private @Getter @Setter boolean recurrente;
     private @Getter @Setter String estado;
     private @Getter @Setter Date solicitud;
     private @Getter @Setter Timestamp inicio;
     private @Getter @Setter Timestamp fin;
+    private @Getter @Setter List<ReservaRecurrente> reservaRecurrentes;
+    private @Getter @Setter List<Usuario> usuarios;
+    private @Getter @Setter List<Recurso> recursos;
 
     @Override
     public String toString(){
-        return "Reserva" + "id=" + id + "inicio" + inicio.toString() + "}";
+        return "Reserva: " + "id = " + id + " inicio: " + inicio.toString() + " Usuario: " + idUsuario.getNombre() + " Recurso: " + idRecurso.getNombre() + "}";
     }
 }
